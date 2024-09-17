57b901eb0867475223d149fc9e5c690a17ffbba4
Victor Castillo <Castillovictor2461@gmail.com>
2024-09-17 11:32:05 -0400

    cambios en el archivo de login

--- api/controllers/login.js
+++ api/controllers/login.js
@@ -5,33 +5,46 @@ const User = require('../models/User')
 const { PRIVATE_KEY } = process.env
 
 loginRouter.post('/', async (req, res) => {
-    const { body } = req
-    const { username, password } = body
+    try {
+        const { username, password } = req.body
 
-    const user = await User.findOne({ username })
-    const passwordCorrect =
-        user === null
-            ? false
-            : await bcrypt.compare(password, user.passwordHash)
+        // Validar que username y password estén presentes
+        if (!username || !password) {
+            return res.status(400).json({
+                error: 'Username or password missing',
+            })
+        }
 
-    if (!(user && passwordCorrect)) {
-        return res.status(401).json({
-            error: 'Invalid username or password',
+        const user = await User.findOne({ username })
+        const passwordCorrect =
+            user === null
+                ? false
+                : await bcrypt.compare(password, user.passwordHash)
+
+        if (!(user && passwordCorrect)) {
+            return res.status(401).json({
+                error: 'Invalid username or password',
+            })
+        }
+
+        const userForToken = {
+            id: user._id,
+            username: user.username,
+        }
+
+        // Generar el token con expiración
+        const token = jwt.sign(userForToken, PRIVATE_KEY, { expiresIn: '1h' })
+
+        // Responder con los datos del usuario y el token
+        res.send({
+            name: user.name,
+            username: user.username,
+            token,
         })
+    } catch (error) {
+        // Manejar errores inesperados
+        res.status(500).json({ error: 'Something went wrong' })
     }
-
-    const userForToken = {
-        id: user._id,
-        username: user.username,
-    }
-
-    const token = jwt.sign(userForToken, PRIVATE_KEY)
-
-    res.send({
-        name: user.name,
-        username: user.username,
-        token,
-    })
 })
 
 module.exports = loginRouter
